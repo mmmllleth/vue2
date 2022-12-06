0e5945547bcf32370b199f9d0b67bf9b355652f2
icpbox <icpbox@protonmail.com>
2022-12-07 00:24:58 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img
+      alt="Vue logo"
+      src="./assets/logo.png"
+    >
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Test />
   </div>
 </template>
 
-<script>
+<script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import Test from "./components/Test.vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
---
+++ src/components/Test.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    test:
+    {{ data.test }}
+  </div>
+</template>
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+const data = reactive({test: 'name'})
+</script>
+
